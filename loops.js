dee1a3de7331c94cb44fc463d22b20dbde3381a1
CMC06 <crzychelc@gmail.com>
2021-05-02 15:17:40 -0400

    solved loop challenges

--- loops.js
+++ loops.js
@@ -1,40 +1,100 @@
 // While Loops
 // write a while loop that logs 'hello world' 1000
-
+let helloCount = 0;
+while(helloCount < 1000){
+  console.log('hello world');
+  helloCount ++;
+}
 // wirte while loop counts 0 to 1000
+let loopCount = 0;
+while(loopCount < 1001){
+  console.log(loopCount);
+  loopCount++;
+}
 
 // wirte a while looop counts 0 to 1000 and concatenates current loop number
-
+let loopConcatCount = 0;
+while(loopConcatCount <= 1000){
+  console.log(`current loop number: ${loopConcatCount}`)
+  loopConcatCount++;
+}
 
 // For Loops
 // wirte a for loop counts 0 to 100
+for(let i = 0; i <= 100; i++){
+  console.log(i);
+}
 
 // wirte for loop counts 7 to 635
+for(let i = 7; i <= 635; i++){
+  console.log(i);
+}
 
-// write a for loop using varialbes
+// write a for loop using variables
 
 
 
 /////////////////////
 // Booleans & Loops
 // create a loop that prints 'Hi' 15 times
+for(let i = 0; i < 15; i++){
+  console.log('Hi');
+}
 
 // create a loop that prints 'YEP!' 30 times
-
+for(let i = 0; i < 30; i++){
+  console.log('YEP!');
+}
 // create a loop count 0-99 ascending
+for(let i = 0; i < 100; i++){
+  console.log(i);
+}
 
 // create a loop count 0-99 descending
-
+for(let i = 99; i >= 0; i--){
+  console.log(i);
+}
 // create a loop logs only EVEN numbers from 0-98 ascending
+for(let i = 0; i < 99; i++){
+  if(i % 2 === 0){
+    console.log(i);
+  }
+}
 
 // create a loop logs EVEN numbers from 98-0 descending
+for(let i = 98; i >= 0; i--){
+  if(i % 2 === 0){
+    console.log(i);
+  }
+}
 
 // create a loop that logs the ODD numbers from 0-99 ascending
+for(let i = 0; i < 100; i++){
+  if(i % 2 !== 0){
+    console.log(i);
+  }
+}
 
 // create a loop that logs ODD nums from 0-99 descending
+for(let i = 99; i >= 0; i --){
+  if(i % 2 !== 0){
+    console.log(i);
+  }
+}
 
 // create a loop that logs nums from 49-72 ascending
+for(let i = 49; i <= 72; i++){
+  console.log(i);
+}
 
 // create a loop that logs nums 81-26 descending
+for(let i = 81; i >= 26; i--){
+  console.log(i);
+}
 
 // create a looop logs nums 3-90 are multiples of 3 ascending
+for(let i = 3; i <= 90; i++){
+  if(i % 3 === 0){
+    console.log(i);
+  }
+}
--- main.js
+++ main.js
@@ -3,7 +3,11 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
-
+  let newStr = '';
+  for(let i = str.length - 1; i >= 0; i--){
+    newStr += str[i];
+  }
+  return newStr;
 };
 console.log(reverseString('hello'));
 
@@ -12,7 +16,13 @@ console.log(reverseString('hello'));
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
 function isPalindrome(str) {
-
+  let reverse = reverseString(str);
+  for(let i = 0; i < str.length; i++){
+    if(str[i] !== reverse[i]){
+      return false;
+    }
+  }
+  return true; 
 }
 console.log(isPalindrome('racecar'));
 
@@ -21,7 +31,15 @@ console.log(isPalindrome('racecar'));
 // ex. reverseInt(521) === 125
 
 function reverseInt(int) {
-
+  let stInt = String(int);
+  let newNum = '';
+  if(int < 0){
+    newNum += '-';
+  }
+  for(let i = stInt.length - 1; i >= 0; i--){
+    newNum += stInt[i];
+  }
+  return parseInt(newNum);
 }
 console.log(reverseInt(-521));
 
@@ -29,7 +47,17 @@ console.log(reverseInt(-521));
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) {
-    
+  newStr = '';
+  for(let i = 0; i < str.length; i++){
+    if(i === 0){
+      newStr += str[i].toUpperCase();
+    } else if (str[i - 1] === ' ') {
+      newStr += str[i].toUpperCase();
+    } else {
+      newStr += str[i];
+    }
+  }  
+  return newStr;
 }
 console.log(capitalizeLetters('i love javascript'));
 
@@ -37,17 +65,41 @@ console.log(capitalizeLetters('i love javascript'));
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
 function maxCharacter(str) {
+  let characters = {};
+  let charEntries = [];
+  for(let i = 0; i < str.length; i++){
+    let letter = str[i];
+    if(characters[letter] === undefined){
+      characters[letter] = 1;
+    } else {
+      characters[letter] += 1;
+    }
+  }
 
+  charEntries = Object.entries(characters);
+  charEntries.sort((a, b) => b[1] - a[1]);
+  let mostCommonChar = charEntries.slice(0, 1);
+  return mostCommonChar[0][0];
 };
 
 console.log(maxCharacter('javascript'));
 
 
-
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 function fizzBuzz() {
-    
+  for(let i = 1; i < 101; i++){
+    if(i % 5 === 0 && i % 3 === 0){
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0){
+      console.log('Fizz');
+    } else if (i % 5 === 0){
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }  
+  return 'I printed to the console.'  
 }
 console.log(fizzBuzz());
 
